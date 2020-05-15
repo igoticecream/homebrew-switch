78047da42dbd8d8536d4051fe1f18abd8593de07
Pedro Diaz <igoticecream@gmail.com>
2020-05-15 18:00:57 +0200

    Update hactool.rb

--- hactool.rb
+++ hactool.rb
@@ -1,8 +1,8 @@
 class Hactool < Formula
   desc "hactool is a tool to view information about, decrypt, and extract common file formats for the Nintendo Switch, especially Nintendo Content Archives."
   homepage "https://github.com/SciresM/hactool"
-  url "https://github.com/SciresM/hactool/archive/1.2.2.tar.gz"
-  sha256 "604e60c6ad2401f71a152a493744d32070019debfb53448bb7a3c571b835ded9"
+  url "https://github.com/SciresM/hactool/archive/1.3.3.tar.gz"
+  sha256 "479939a5c9040059d71ee10847eb4f113cbef5f81a42c2f629772829eb71f89f"
 
   def install
     system "mv", "config.mk.template", "config.mk"
